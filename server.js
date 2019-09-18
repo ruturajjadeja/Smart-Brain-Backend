83d21921deca8aa412c558158525393971bfd64b
Ruturaj <ruturajjadeja21@gmail.com>
2019-09-18 09:52:21 -0500

    changing response

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send(database.users);
+  res.send('It is working!');
 });
 
 app.post('/signin', (req, res) => {
